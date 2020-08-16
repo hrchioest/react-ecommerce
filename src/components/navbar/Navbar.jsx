80f14efcb0abeb01aace6782f53fe583f0a165ea
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-16 01:31:42 -0300

    se cambia de extension al componente home

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CartIconCount from "../carIconCount/CartIconCount";
+import useStyles from "./Style";
+
+const Navbar = ({counter}) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            LOGO
+          </Typography> 
+          <CartIconCount cartCount={counter}/>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/navbar/Style.js
@@ -0,0 +1,13 @@
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const style = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1
+    },
+    title: {
+      flexGrow: 1
+    }
+}));
+
+export default style;
