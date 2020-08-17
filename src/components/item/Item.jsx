dba1b6ea576a54bbca5737383cd05fe89e4a7323
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-17 17:10:14 -0300

    Se agrega listado de productos con propiedades

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import Box from '@material-ui/core/Box';
 
 
-
-const Item = ({img}) =>{
-
-    return(
-        <>
-            <img src={img} alt="img" width="400"></img>
-        </>
-    )
-}
+const Item = ({name, description, img, stock}) => (
+    <Box flexDirection="colum"> 
+        <h2>{name}</h2>
+        <img src={img} alt="img" width="400" />
+        <p>{description}</p>
+        <p>Stock disponible: {stock}und</p>
+    </Box>
+);
 
 export default Item;
--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -1,35 +1,56 @@
-import React from "react";
-import Item from "../item/Item";
+import React, { useState, useEffect } from "react";
 import Box from '@material-ui/core/Box';
-import { useState } from "react";
-import { useEffect } from "react";
 
-const imgs = [
-    "https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-91-99d1fa9eb7125e47b915718462204871-480-0.jpg",
-    "https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-141-17d52c3880823e17f615718526349866-480-0.jpg",
-    "https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-11-29d75e057330bb437c15706249551096-480-0.jpg"];
+import Item from "../item/Item";
 
-const imgList = new Promise ((result, reject) =>{
-    setTimeout(()=>{
-        result(imgs)
-    }, 2000)
+const products = [
+    {
+        id:1,
+        name:"Laptop",
+        description:"MacBook Pro 13'",
+        stock:28,
+        img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-141-17d52c3880823e17f615718526349866-480-0.jpg"
+
+    },
+    {
+        id:2,
+        name:"Watch",
+        description:"White Smartwatch",
+        stock:30,
+        img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-11-29d75e057330bb437c15706249551096-480-0.jpg"
+
+    },
+    {
+        id:3,
+        name:"Airpods",
+        description:"Wireless headphones",
+        stock:25,
+        img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-91-99d1fa9eb7125e47b915718462204871-480-0.jpg"
+
+    }
+]
+
+const listProducts = new Promise ((result, reject) => {
+    setTimeout(() => result(products), 2000)
 });
 
-const ItemList =() =>{
+const ItemList =() => {
 
-    const [list,setList ] = useState([]);
+    const [list, setList] = useState([]);
    
-    useEffect(()=>{
-        imgList.then(res=>{
-            setList(res)
-        })
-    }, [])
+    useEffect(() => listProducts.then(setList), []);
 
     return(
-        <Box display="flex" justifyContent="space-around">
-            {list.map((img,index) =><Item key={index} img={img} />)}
-        </Box>
-    )
-}
+    <Box display="flex" justifyContent="space-around" flexWrap="wrap">
+        {list.map(products => 
+            <Item key={products.id} 
+                img={products.img} 
+                name={products.name}
+                description={products.description}
+                stock={products.stock}
+            />
+        )}
+    </Box>
+)}
 
 export default ItemList;
