23f6484442a2c46c0d1e439fec8a81b2d7880020
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-22 08:34:01 -0300

    Se mueve la lista de productos a un nuevo archivo

---
+++ src/listProducts.js
@@ -0,0 +1,29 @@
+
+const products = [
+    {
+        id:1,
+        name:"Laptop",
+        description:"MacBook Pro 13'",
+        stock:28,
+        img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-141-17d52c3880823e17f615718526349866-480-0.jpg"
+
+    },
+    {
+        id:2,
+        name:"Watch",
+        description:"White Smartwatch",
+        stock:30,
+        img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-11-29d75e057330bb437c15706249551096-480-0.jpg"
+
+    },
+    {
+        id:3,
+        name:"Airpods",
+        description:"Wireless headphones",
+        stock:25,
+        img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-91-99d1fa9eb7125e47b915718462204871-480-0.jpg"
+
+    }
+]
+
+export default products;
