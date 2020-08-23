86e92ba7ce1e5799a31276aa0cda2338f80c02f3
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-23 18:14:16 -0300

    Se acomoda las propiedades del componente Item dentro del componente ItemList

--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -1,27 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Box from '@material-ui/core/Box';
 import Item from "../item/Item";
-import products from "../../listProducts";
 
+const ItemList =({list}) => {
 
-
-const listProducts = new Promise ((result, reject) => setTimeout(() => result(products), 2000))
     
-
-const ItemList =() => {
-
-    const [list, setList] = useState([]);
-   
-    useEffect(() => listProducts.then(setList), []);
-
     return(
     <Box display="flex" justifyContent="space-around" flexWrap="wrap">
-        {list.map(products => 
-            <Item key={products.id} 
-                img={products.img} 
-                name={products.name}
-                description={products.description}
-                stock={products.stock}
+        {list.map(product => 
+            <Item key={product.id} detail={product} 
             />
         )}
     </Box>
