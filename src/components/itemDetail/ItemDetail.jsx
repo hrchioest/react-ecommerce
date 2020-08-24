0ad66089492936a920c44e2d35a3aea9aec93f33
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-24 12:20:51 -0300

    se acomoda estilos

--- src/components/itemDetail/ItemDetail.jsx
+++ src/components/itemDetail/ItemDetail.jsx
@@ -5,24 +5,22 @@ import { Button } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 
 
-const ItemDetail = ({detail, handleAdd}) => {
+const ItemDetail = ({item, handleAdd}) => {
 
     return(
+    
         <Box display="flex" justifyContent="center" margin="auto" flexWrap="wrap" bgcolor="grey.300" m={2}>
-            <Item detail={detail} />
+            <Item item={item} />
             <Box margin={10}>
-                <div> 
                     <ItemCount 
                     onAdd ={handleAdd}
                     initial ={1}   
                     min={1}
                     max={30}/>
-                </div>
-                <div>
-                    <Button variant="contained" color="primary" fullWidth="100%">Comprar</Button>
-                </div>
+                    <Button variant="contained" color="primary" fullWidth >Comprar</Button>
             </Box>
         </Box>
+
     );  
 }
 
