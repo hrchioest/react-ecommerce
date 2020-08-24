7e38af3b7b01fb1bd465dea8802072153614b3a1
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-24 12:23:12 -0300

    se retira de la promesa el reject

--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -3,15 +3,15 @@ import Box from '@material-ui/core/Box';
 import Item from "../item/Item";
 
 const ItemList =({list}) => {
-
-    
     return(
-    <Box display="flex" justifyContent="space-around" flexWrap="wrap">
-        {list.map(product => 
-            <Item key={product.id} detail={product} 
-            />
-        )}
-    </Box>
-)}
+        <Box display="flex" justifyContent="space-around" flexWrap="wrap">
+            {list.map(product => 
+                <Item key={product.id} item={product} 
+                />
+                
+            )}
+        </Box>
+    )
+}
 
 export default ItemList;
--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -2,10 +2,9 @@ import React, {useState, useEffect} from 'react';
 import ItemList from '../itemList/ItemList';
 import products from "../../listProducts";
 
-const listProducts = new Promise (
-    (result, reject) => setTimeout(() => result(products), 2000)
+const listProducts = new Promise ((result) => setTimeout(() => result(products), 2000)
 );
-    
+
 const ItemListContainer = () =>{
     
     const [list, setList] = useState([]);
@@ -13,8 +12,8 @@ const ItemListContainer = () =>{
     useEffect(() => {
         listProducts.then(setList)
     }, [])
-
     return(
+       
         <ItemList list={list} />
     )
 }
