9772556b6290b906d92b833df2780b5404b4de5c
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-30 13:41:29 -0300

    se agrega nuevo estado cartItem

--- src/App.js
+++ src/App.js
@@ -3,25 +3,41 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
 import ItemDetailContainer from './components/itemDetailContainer/ItemDetailContainer';
+import ContextCart from './cartContext';
+
 
 function App() {
   const [cartCount, setCartCount] = useState(0);
-
+  const [cartItem, setCartItem] = useState([]);
   const handleAdd = (count) =>{
+    console.log('123');
     setCartCount(count)
   }
+  const productsAdd = (itemCount) =>{
+
+    setCartItem(state => {
+      return [...state, itemCount]
+    })
+    
+  }
+
+  console.log('state cartime: ',cartItem);
+
+
   return (
-    <BrowserRouter>
-        <Navbar counter={cartCount}/>
-      <Switch>
-        <Route exact path = "/">
-          <Home />
-        </Route>
-        <Route exact path = "/item/:productId">
-        <ItemDetailContainer handleAdd={handleAdd}/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <ContextCart.Provider value={{cartCount, handleAdd, productsAdd}}>
+      <BrowserRouter>
+          <Navbar />
+        <Switch>  
+          <Route exact path = "/">
+            <Home />
+          </Route>
+          <Route exact path = "/item/:productId">
+          <ItemDetailContainer />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </ContextCart.Provider>
   );
 }
 
