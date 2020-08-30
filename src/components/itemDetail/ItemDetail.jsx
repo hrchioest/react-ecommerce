3f649ae7bacb354ba1eca738e45fe28bed4b3930
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-30 18:12:02 -0300

    se usa el useState de count para pasarle por parametro en itemCount y se agrega la función al boton comprar

--- src/components/itemDetail/ItemDetail.jsx
+++ src/components/itemDetail/ItemDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ItemCount from '../itemCount/ItemCount';
 import Item from '../item/Item';
 import { Button } from '@material-ui/core';
@@ -7,17 +7,26 @@ import Box from '@material-ui/core/Box';
 
 const ItemDetail = ({item, handleAdd}) => {
 
+    const [count, setCount] = useState(1);
+
+    const handleClick = () =>{
+        handleAdd(count)
+    }
     return(
     
         <Box display="flex" justifyContent="center" margin="auto" flexWrap="wrap" bgcolor="grey.300" m={2}>
             <Item item={item} />
             <Box margin={10}>
                     <ItemCount 
-                    onAdd ={handleAdd}
-                    initial ={1}   
-                    min={1}
-                    max={30}/>
-                    <Button variant="contained" color="primary" fullWidth >Comprar</Button>
+                        setCount={setCount}
+                        count={count} 
+                        min={1}
+                        max={30}/>
+                    <Button variant="contained"
+                        color="primary"
+                        onClick ={handleClick} 
+                        fullWidth >Comprar
+                    </Button>
             </Box>
         </Box>
 
