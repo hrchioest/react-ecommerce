bcc1d801c106e672bff9618d224bece1d253cfdc
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-09-07 18:24:58 -0300

    se retira menu de ui y se agrega una tabla para mostrar img, producto, cantidad y precio

--- src/components/cart/CartIcon.jsx
+++ src/components/cart/CartIcon.jsx
@@ -1,34 +1,30 @@
-import React, {useState} from 'react';
+import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import Cart from '../cart/Cart';
+import { Link } from 'react-router-dom';
 import CartContext from '../../CartContext';
 
 
 const CartIcon = () =>{
 
-    const [anchorEl, setAnchorEl] = useState(false);
+    
     const { cartItem } = React.useContext(CartContext);
     
-    const handleClick = (event) =>{
-        setAnchorEl(event.currentTarget) 
-    }
-
+   
     const cantTotal = () =>{
         return cartItem.reduce((acumulador, item) => acumulador + item.count, 0);        
     }
    
     return(
         <>
-            <div aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            <Link to="/cart">
                 <IconButton >
                     <Badge badgeContent={cantTotal()}  color="secondary" showZero>
                         <ShoppingCartIcon />
                     </Badge>
                 </IconButton>
-            </div>
-            <Cart anchorEl={anchorEl} setAnchorEl={setAnchorEl}/>
+            </Link>
         </>
 
         
