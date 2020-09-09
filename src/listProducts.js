e02a0932f5b62964d84e93b2d19919e46c92e341
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-09-09 11:24:54 -0300

    se agrega stock en la lista de productos

--- src/listProducts.js
+++ src/listProducts.js
@@ -5,21 +5,24 @@ const products = [
         name:"Laptop",
         description:"MacBook Pro 13'",
         img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-141-17d52c3880823e17f615718526349866-480-0.jpg",
-        price:"$89.999,00"
+        price:"$89.999,00",
+        stock:95
     },
     {
         id:2,
         name:"Watch",
         description:"White Smartwatch",
         img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-11-29d75e057330bb437c15706249551096-480-0.jpg",
-        price:"$8.999,00"
+        price:"$8.999,00",
+        stock:80
     },
     {
         id:3,
         name:"Airpods",
         description:"Wireless headphones",
         img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-91-99d1fa9eb7125e47b915718462204871-480-0.jpg",
-        price:"$24.900,00"
+        price:"$24.900,00",
+        stock:100
     }
 ]
 
