75f4ce10782344b03d00d2053d2e365f165a9a68
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-09-09 21:01:29 -0300

    se agrega a la lista categoryId

--- src/listProducts.js
+++ src/listProducts.js
@@ -2,7 +2,7 @@
 const products = [
     {
         id:1,
-        name:"Laptop",
+        categoryId:"Laptop",
         description:"MacBook Pro 13'",
         img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-141-17d52c3880823e17f615718526349866-480-0.jpg",
         price:"$89.999,00",
@@ -10,7 +10,7 @@ const products = [
     },
     {
         id:2,
-        name:"Watch",
+        categoryId:"Watch",
         description:"White Smartwatch",
         img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-11-29d75e057330bb437c15706249551096-480-0.jpg",
         price:"$8.999,00",
@@ -18,7 +18,7 @@ const products = [
     },
     {
         id:3,
-        name:"Airpods",
+        categoryId:"Airpods",
         description:"Wireless headphones",
         img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-91-99d1fa9eb7125e47b915718462204871-480-0.jpg",
         price:"$24.900,00",
