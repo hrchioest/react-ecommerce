cc46f97cb76bc4c7ec0d9948144eb6ed7466532c
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-09-09 21:59:20 -0300

    Se agrega mas productos

--- src/listProducts.js
+++ src/listProducts.js
@@ -2,7 +2,8 @@
 const products = [
     {
         id:1,
-        categoryId:"Laptop",
+        categoryId:"notebooks",
+        name:"MacBook",
         description:"MacBook Pro 13'",
         img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-141-17d52c3880823e17f615718526349866-480-0.jpg",
         price:"$89.999,00",
@@ -10,7 +11,8 @@ const products = [
     },
     {
         id:2,
-        categoryId:"Watch",
+        categoryId:"smartwatches",
+        name:"Watch",
         description:"White Smartwatch",
         img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-11-29d75e057330bb437c15706249551096-480-0.jpg",
         price:"$8.999,00",
@@ -18,11 +20,30 @@ const products = [
     },
     {
         id:3,
-        categoryId:"Airpods",
+        categoryId:"audio",
+        name:"Airpods",
         description:"Wireless headphones",
         img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-91-99d1fa9eb7125e47b915718462204871-480-0.jpg",
         price:"$24.900,00",
         stock:100
+    }
+    {
+        id:4,
+        categoryId:"audio",
+        name:"Auriculares",
+        description:"Auriculares Bluetooth",
+        img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-211-92f8ed128ca3c0e90d15706313429025-480-0.jpg",
+        price:"$24.900,00",
+        stock:100
+    }
+    {
+        id:5,
+        categoryId:"notebooks",
+        name:"Notebook",
+        description:"Notebook Sansung 7 15.6",
+        img:"https://d26lpennugtm8s.cloudfront.net/stores/001/063/033/products/prod-331-e3762d197cf7bf001815718530222960-480-0.jpg",
+        price:"$24.900,00",
+        stock:85
     }
 ]
 
