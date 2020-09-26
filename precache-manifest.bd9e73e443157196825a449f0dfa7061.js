5e2ace0489659c01811e48cd641c2db843736fbe
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-09-26 19:01:29 -0300

    Updates

---
+++ precache-manifest.bd9e73e443157196825a449f0dfa7061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bad55d6d195dd17475c0acfa3c391f",
+    "url": "/react-ecommerce/index.html"
+  },
+  {
+    "revision": "28eb803fcc3c9dd1b577",
+    "url": "/react-ecommerce/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "22ee7930dd3395544b5a",
+    "url": "/react-ecommerce/static/js/2.72fb6b4a.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/react-ecommerce/static/js/2.72fb6b4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28eb803fcc3c9dd1b577",
+    "url": "/react-ecommerce/static/js/main.049b326d.chunk.js"
+  },
+  {
+    "revision": "167a59a0d7a2e54230c3",
+    "url": "/react-ecommerce/static/js/runtime-main.d6cc4b0e.js"
+  }
+]);
