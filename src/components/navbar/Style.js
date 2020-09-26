71a33bd2c1bfdd842c8cde38b3b302dc2ea6a0d9
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-09-26 17:09:20 -0300

    se hace ajustes de estilos en navbar

--- src/components/navbar/Style.js
+++ src/components/navbar/Style.js
@@ -1,13 +1,22 @@
-
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles'
 
 const style = makeStyles((theme) => ({
     root: {
-      flexGrow: 1
+        flexGrow: 1,
     },
     title: {
-      flexGrow: 1
-    }
-}));
+        flexGrow: 1,
+        padding: '7px',
+        height: '43px',
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 180,
+    },
+    cat: {
+        color: 'black',
+        textDecoration: 'none',
+    },
+}))
 
-export default style;
+export default style
