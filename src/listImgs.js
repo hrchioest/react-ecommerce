4dd1148005f5b0cf8ce573f550fdeb83cf7abe9b
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-03-16 21:44:56 -0300

    se agrega  un listado de imagenes hero por cada categoría

---
+++ src/listImgs.js
@@ -0,0 +1,19 @@
+const imgs = [
+    {
+        idCategory: 'notebooks',
+        img:
+            'https://images.unsplash.com/photo-1544731612-de7f96afe55f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+        idCategory: 'audio',
+        img:
+            'https://images.unsplash.com/photo-1528148343865-51218c4a13e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+        idCategory: 'smartwatches',
+        img:
+            'https://images.unsplash.com/photo-1434493789847-2f02dc6ca35d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80',
+    },
+]
+
+export default imgs
