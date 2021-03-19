b1befc59c1909edd5063b3f538ba8f6b17f8e0e8
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-03-19 10:11:36 -0300

    Se agrega contenido en quienes somos

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -2,22 +2,27 @@ import React from 'react'
 import { Box, Button } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 
-const Item = ({ item: { id, name, description, img } }) => (
+const Item = ({ item: { id, name, description, price, img } }) => (
     <Box flexDirection="colum" marginTop="60px">
         <h2>{name}</h2>
-        <Link to={`/item/${id}`}>
-            <img src={img} alt="img" width="350" />
-        </Link>
-        <Box>
+        <span>Price :{price}</span>
+
+        <div>
             <Link to={`/item/${id}`}>
-                <Button
-                    variant="contained"
-                    style={{ backgroundColor: '#f7d04b' }}
-                >
-                    Details
-                </Button>
+                {' '}
+                <img src={img} alt="img" width="350" />
             </Link>
-        </Box>
+        </div>
+
+        <Button variant="contained" style={{ backgroundColor: '#f7d04b' }}>
+            <Link
+                to={`/item/${id}`}
+                style={{ textDecoration: 'none', color: 'black' }}
+            >
+                {' '}
+                Details{' '}
+            </Link>
+        </Button>
     </Box>
 )
 
