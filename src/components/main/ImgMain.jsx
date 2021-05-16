3aa5180fdd46da9eb8119cea955295e4e05e8074
hrchioest <hr.chio@gmail.com>
2021-05-16 11:37:22 -0300

    ajuste de estilo

--- src/components/main/ImgMain.jsx
+++ src/components/main/ImgMain.jsx
@@ -6,7 +6,7 @@ const ImgMain = () => {
             <img
                 src="https://d319yleido6tgg.cloudfront.net/stores/001/063/033/themes/cubo/slide-1571835719477-7385694174-9537d0d86060d32ec8c5dc6d975ab9741571835723-1920-1920.jpg?755810131"
                 alt="sections"
-                style={{ width: '100%', height: '350px', objectFit: 'cover' }}
+                style={{ width: '100%', height: '450px', objectFit: 'cover' }}
             />
         </div>
     )
